2438d08e07b654229b6c16c7f9999cc3c5688898
James Ye <jamesye105@yahoo.ca>
2020-05-27 09:53:21 -0400

    Separated functions to separate .c file; fix memory leaks

--- lab1/starter/catpng.c
+++ lab1/starter/catpng.c
@@ -6,147 +6,6 @@
 #include "png_util/lab_png.h"   /* simple PNG data structures  */
 #include "png_util/zutil.h"     /* for mem_def() and mem_inf() */
 
-#define BASE 256
-
-/* Function to calculate values because the one from math.h doesn't work */
-int power(int base, int exp){
-    int result = 1;
-    for(int i = 0; i < exp; i++){
-        result *= base;
-    }
-    return result;
-}
-
-/* Used with power() to calculate values from buffer */
-U32 getValue(unsigned char *buf, int base, int start, int end){
-    int retVal = 0;
-    int exp = end - start - 1;
-
-    for(int i = start; i < end; i++){
-        retVal += buf[i] * power(base, exp);
-        exp--;
-    }
-    return retVal;
-}
-
-/* Looks at file size to see how much space needs to be allocated to buffer */
-U32 getBufferSize(U8 *path){
-    FILE *png = fopen(path, "rb");
-    if(png == NULL){
-            printf("couldnt find png\n");
-            return -1;
-    }
-
-    fseek(png, 0L, SEEK_END);
-    return ftell(png);
-}
-
-int is_png(U8 *buf){
-    U8 signature[8] = {0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A};
-    U8 header[8];
-    for(int i = 0; i < 8; i++){
-        header[i] = buf[i];
-    }
-    return !memcmp(signature, header, sizeof(signature));
-}
-
-int get_png_width(struct chunk *ihdr){
-    return getValue(ihdr->p_data, BASE, 0, 4);
-}
-
-int get_png_height(struct chunk *ihdr){
-    return getValue(ihdr->p_data, BASE, 4, 8);
-}
-
-U8 *insertValue(U32 value, int size){
-    U8 *retVal = malloc(size);
-    // printf("blah %x\n", value);
-    for(int i = size - 1; i >= 0; i--){
-        retVal[i] = value % 256;
-        value /= 256;
-    }
-    return retVal;
-}
-
-simple_PNG_p createPNG(U8 *path){
-    simple_PNG_p retVal = malloc(3 * sizeof(struct simple_PNG));
-    U32 buffer_size = getBufferSize(path);
-    U8* buffer = malloc(buffer_size);
-    FILE *png = fopen(path, "rb");
-    if(png == NULL){
-		return NULL;
-    }
-    memset(buffer, 0, buffer_size);
-    fread(buffer, buffer_size, 1, png);
-    int isPNG = is_png(buffer);
-    if(!isPNG){
-        return NULL;
-    }
-
-    chunk_p ihdr = malloc(sizeof(struct chunk));
-    chunk_p idat = malloc(sizeof(struct chunk));
-    chunk_p iend = malloc(sizeof(struct chunk));
-
-    /* Getting values for ihdr chunk */
-    int ihdr_start = 8;
-    U8 ihdr_type[4];
-    U32 ihdr_length = getValue(buffer, BASE, ihdr_start, ihdr_start + 4);
-    int index = 0;
-    /* ihdr type bytes should be 12 to 15 always */
-    for(int i = ihdr_start + 4; i < ihdr_start + 8; i++, index++){
-        ihdr_type[index] = buffer[i];
-    }
-    U32 ihdr_crc = getValue(buffer, BASE, ihdr_start + ihdr_length + 8, ihdr_start + ihdr_length + 12);
-
-    /* Getting values for idat chunk */
-    int idat_start = ihdr_start + ihdr_length + 12;
-    U8 idat_type[4];
-    U32 idat_length = getValue(buffer, BASE, idat_start, idat_start + 4);
-    index = 0;
-    for(int i = idat_start + 4; i < idat_start + 8; i++, index++){
-        idat_type[index] = buffer[i];
-    }
-    U32 idat_crc = getValue(buffer, BASE, idat_start + idat_length + 8, idat_start + idat_length + 12);
-
-    /* Getting values for iend chunk */
-    int iend_start = idat_start + idat_length + 12;
-    U8 iend_type[4];
-    U32 iend_length = getValue(buffer, BASE, iend_start, iend_start + 4);
-    index = 0; 
-    for(int i = iend_start + 4; i < iend_start + 8; i++, index++){
-        iend_type[index] = buffer[i];
-    }
-    U32 iend_crc = getValue(buffer, BASE, iend_start + 8, iend_start + 12);
-
-    ihdr->length = ihdr_length;
-    memcpy(ihdr->type, ihdr_type, sizeof(ihdr_type));
-    ihdr->p_data = &buffer[ihdr_start + 8];
-    ihdr->crc = ihdr_crc;
-
-    idat->length = idat_length;
-    memcpy(idat->type, idat_type, sizeof(idat_type));
-    idat->p_data = &buffer[idat_start + 8];
-    idat->crc = idat_crc;
-
-    iend->length = iend_length;
-    memcpy(iend->type, iend_type, sizeof(iend_type));
-    iend->p_data = NULL;
-    iend->crc = iend_crc;
-    // printf("blah %x\n", iend_crc);
-
-    U8 *buf = (U8 *)malloc(sizeof(U8) * buffer_size);
-    for(int i = 0; i < buffer_size; i++){
-        buf[i] = buffer[i];
-    }
-
-    // retVal->buffer = buf;
-    retVal->p_IHDR = ihdr;
-    retVal->p_IDAT = idat;
-    retVal->p_IEND = iend;
-
-    return retVal;
-}
-
 int main(int argc, char *argv[]){
     /* Get path for png */
     if (argc == 1) {
@@ -163,18 +22,13 @@ int main(int argc, char *argv[]){
         }
     }
 
-    simple_PNG_p cat = malloc(3 * sizeof(struct simple_PNG));
-
     int width, height = 0;
 
     width = get_png_width(pngs[0]->p_IHDR);
-    // printf("Width: %d\n", width);
 
     for(int i = 0; i < argc - 1; i++){
         height += get_png_height(pngs[i]->p_IHDR);
     }
-
-    // printf("Height: %d\n", height);
 
     int final_size = height * (width * 4 + 1);
     U8 *final_buffer = malloc(final_size);
@@ -187,7 +41,7 @@ int main(int argc, char *argv[]){
         // printf("original len = %d, len_def = %lu, len_inf = %lu\n", \
                final_size, pngs[i]->p_IDAT->length, output_length);
         } else { /* failure */
-            // fprintf(stderr,"mem_def failed. ret = %d.\n", ret);
+            fprintf(stderr,"mem_def failed. ret = %d.\n", ret);
         }
         offset += output_length;
     }
@@ -200,7 +54,7 @@ int main(int argc, char *argv[]){
     if (ret == 0) { /* success */
         // printf("original len = %d, len_def = %lu\n");
     } else { /* failure */
-        // fprintf(stderr,"mem_def failed. ret = %d.\n", ret);
+        fprintf(stderr,"mem_def failed. ret = %d.\n", ret);
         return ret;
     }
 
@@ -291,6 +145,25 @@ int main(int argc, char *argv[]){
     fwrite(concat_png, 1, sizeof(concat_png), fp);
 
     fclose(fp);
+
+    for(int i = 0; i < argc - 1; i++){
+        freePNG(pngs[i]);
+    }
+
+    free(new_height);
+    free(new_ihdr_length);
+    free(new_ihdr_crc);
+    free(new_idat_length);
+    free(new_idat_crc);
+    free(new_iend_length);
+    free(new_iend_crc);
+
+    free(ihdr_buf);
+    free(idat_buf);
+    free(iend_buf);
+    free(decompressed_buffer);
+    free(final_buffer);
+    free(new_ihdr_data);
 
     return 0;
 }

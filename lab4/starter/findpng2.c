732d64d885e3c49d7c9abd6d8ba66769a7da9ade
freeman-zhang <freemanzhang2@gmail.com>
2020-07-04 18:10:11 -0400

    setting stuff up

--- lab4/starter/findpng2.c
+++ lab4/starter/findpng2.c
@@ -1,44 +1,148 @@
-#include <stdio.h>            /* printf needs to include this header file */
-#include <stdlib.h>           /* for malloc()                 */
-#include <unistd.h>           /* for getopt                   */
-#include <getopt.h>           /* to get rid of optarg linting */
-#include <curl/curl.h>        /* for cURL                     */
+#include <stdio.h>  /* printf needs to include this header file */
+#include <stdlib.h> /* for malloc()                 */
+#include <unistd.h> /* for getopt                   */
+#include <getopt.h> /* to get rid of optarg linting */
+#include <string.h>
+//#include <curl/curl.h>        /* for cURL                     */
 #include <pthread.h>          /* for pthread                  */
 #include "png_util/lab_png.h" /* simple PNG data structures   */
-#include "cURL/curl_util.h"   /* for header and write cb      */
+//#include "cURL/curl_util.h"   /* for header and write cb      */
 
-void crawler(void *url)
+typedef struct char_queue
 {
-    //dq a url from frontier
+    int front;
+    int rear;
+    int count;
+    int max;
+    char **url;
+} * char_queue_p;
 
-    //curl call to url
+int empty(png_queue_p queue)
+{
+    return queue->count == 0;
+}
 
-    //use libxml to traverse page,
-    //check links against pages visted:
+char *dequeue(char_queue_p queue)
+{
+    char *retVal;
+    if (!empty(queue))
+    {
+        retVal = &queue->url[queue->front++];
+
+        if (queue->front == queue->max)
+        {
+            queue->front = 0;
+        }
+        queue->count--;
+    }
+    else
+    {
+        retVal = NULL;
+    }
+    return retVal;
+}
+
+int enqueue(char_queue_p queue, struct recv_buf buf)
+{
+    if (!full(queue))
+    {
+        if (queue->rear == queue->max - 1)
+        {
+            queue->rear = -1;
+        }
+        queue->rear = queue->rear + 1;
+        memcpy(queue->url[queue->rear].entry, buf.buf, buf.max_size);
+        queue->url[queue->rear].number = buf.seq;
+
+        queue->count++;
+    }
+    else
+    {
+        return -1;
+    }
+    return 0;
+}
+
+void *crawler(void *url)
+{
+    // dq a url from frontier
+
+    // curl call to url
+
+    // use libxml to traverse page,
+    // check links against pages visted:
     // if on list, then ignore
     // if not on list, then add to frontier
 
-    //check pngs against pngs list:
+    // check pngs against pngs list:
     // if on list, ignore
     // if not on list and pngs list not full, add to png list
 
-    //exit conditions:
+    // exit conditions:
     // All threads are done and frontier empty
     // pngs full
 
-    //repeat
+    // repeat
 
-    //on exit, cleanup
+    // on exit, cleanup
 }
+
+int process_data(CURL *curl_handle, RECV_BUF *p_recv_buf)
+{
+    CURLcode res;
+    pid_t pid = getpid();
+    long response_code;
+
+    res = curl_easy_getinfo(curl_handle, CURLINFO_RESPONSE_CODE, &response_code);
+    if (res == CURLE_OK)
+    {
+        printf("Response code: %ld\n", response_code);
+    }
+
+    if (response_code >= 400)
+    {
+        fprintf(stderr, "Error.\n");
+        return 1;
+    }
+
+    char *ct = NULL;
+    res = curl_easy_getinfo(curl_handle, CURLINFO_CONTENT_TYPE, &ct);
+    if (res == CURLE_OK && ct != NULL)
+    {
+        printf("Content-Type: %s, len=%ld\n", ct, strlen(ct));
+    }
+    else
+    {
+        fprintf(stderr, "Failed obtain Content-Type\n");
+        return 2;
+    }
+
+    if (strstr(ct, CT_HTML))
+    {
+        // check links against pages visted:
+        // if on list, then ignore
+        // if not on list, then add to frontier
+    }
+    else if (strstr(ct, CT_PNG))
+    {
+        // check pngs against pngs list:
+        // if on list, ignore
+        // if not on list and pngs list not full, add to png list
+    }
+
+    return 0;
+}
+
+char_queue_p frontier;
 
 int main(int argc, char **argv)
 {
     int c;
     int num_threads = 1;
-    int num_urls = 50;
-    char logfile = "";
+    int num_pngs = 50;
+    char logfile[100] = "";
     /* Maybe replace this with a call to main_getopt.c */
-    while ((c = getopt(argc, argv, "t:m:")) != -1)
+    while ((c = getopt(argc, argv, "t:m:v:")) != -1)
     {
         switch (c)
         {
@@ -51,22 +155,64 @@ int main(int argc, char **argv)
             }
             break;
         case 'm':
-            num_urls = strtoul(optarg, NULL, 10);
-            if (num_urls <= 0)
+            num_pngs = strtoul(optarg, NULL, 10);
+            if (num_pngs <= 0)
             {
                 fprintf(stderr, "%s: -m > 0 -- 'n'\n", argv[0]);
                 return -1;
             }
+            break;
+        case 'v':
+            strcpy(logfile, optarg);
             break;
         default:
             return -1;
         }
     }
 
+    frontier = malloc(sizeof(4 * int) + sizeof(100 * 256 * char));
+
+    char seedurl[256];
+    strcpy(seedurl, argv[argc - 1]);
+
+    // printf("num threads = %d\n", num_threads);
+    // printf("num pngs = %d\n", num_pngs);
+    // printf("logfile = %s\n", logfile);
+    // printf("seed = %s\n", seedurl);
+
+    CURL *curl_handle;
+    CURLcode res;
+    RECV_BUF recv_buf;
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl_handle = easy_handle_init(&recv_buf, seedurl);
+    if (curl_handle == NULL)
+    {
+        fprintf(stderr, "Curl initialization failed. Exiting...\n");
+        curl_global_cleanup();
+        abort();
+    }
+
+    res = curl_easy_perform(curl_handle);
+
+    if (res != CURLE_OK)
+    {
+        fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        cleanup(curl_handle, &recv_buf);
+        exit(1);
+    }
+
+    process_data(curl_handle, &recv_buf);
+
+    //curl
     //use libxml, traverse the seed url, set up frontier
     //add seed url to pages visited
 
     //create threads
-
+    // pthread_t crawlers[num_threads];
+    // for (int i = 0; i < num_threads; i++)
+    // {
+    //     pthread_create(&crawlers[i], NULL, crawler, NULL);
+    // }
     return 0;
 }
---
+++ lab4/starter/png_util/lab_png.c
@@ -0,0 +1,297 @@
+#include <string.h>
+#include <stdlib.h>
+
+#include "crc.h"       
+#include "zutil.h"
+#include "lab_png.h"
+
+/* Function to calculate values because the one from math.h doesn't work */
+int power(int base, int exp){
+    int result = 1;
+    for(int i = 0; i < exp; i++){
+        result *= base;
+    }
+    return result;
+}
+
+/* Used with power() to calculate values from buffer */
+U32 getValue(unsigned char *buf, int base, int start, int end){
+    int retVal = 0;
+    int exp = end - start - 1;
+
+    for(int i = start; i < end; i++){
+        retVal += buf[i] * power(base, exp);
+        exp--;
+    }
+    return retVal;
+}
+
+/* Looks at file size to see how much space needs to be allocated to buffer */
+U32 getBufferSize(U8 *path){
+    FILE *png = fopen((char*)path, "rb");
+    if(png == NULL){
+        return -1;
+    }
+    fseek(png, 0L, SEEK_END);
+    return ftell(png);
+}
+
+/* Checks if the file's header matches that of a png */
+int is_png(U8 *buf){
+    U8 signature[8] = {0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A};
+    U8 header[8];
+    for(int i = 0; i < 8; i++){
+        header[i] = buf[i];
+    }
+    return !memcmp(signature, header, sizeof(signature));
+}
+
+int get_png_width(struct chunk *ihdr){
+    return getValue(ihdr->p_data, 256, 0, 4);
+}
+
+int get_png_height(struct chunk *ihdr){
+    return getValue(ihdr->p_data, 256, 4, 8);
+}
+
+/* Frees all heap allocated memory from a png */
+void freePNG(simple_PNG_p png){
+    free(png->p_IHDR->p_data);
+    free(png->p_IHDR);
+    free(png->p_IDAT->p_data);
+    free(png->p_IDAT);
+    free(png->p_IEND);
+    free(png);
+}
+
+/**
+ * @brief returns 'size' length unsigned char representing 'value'
+ * @param value unsigned int, value to be converted
+ * @param size  int, desired size of array
+ */
+U8 *insertValue(U32 value, int size){
+    U8 *retVal = malloc(size);
+    for(int i = size - 1; i >= 0; i--){
+        retVal[i] = value % 256;
+        value /= 256;
+    }
+    return retVal;
+}
+
+/**
+ * @brief returns a simple_PNG_p
+ * @param buffer unsigned char *, array holding data of png
+ * @param buffer_size  unsigned int, size of buffer
+ */
+
+simple_PNG_p createPNG(U8 *buffer){
+    simple_PNG_p retVal = malloc(3 * sizeof(struct simple_PNG));
+    int isPNG = is_png(buffer);
+    if(!isPNG){
+        retVal->errCode = -2;
+        return retVal;
+    }
+
+    chunk_p ihdr = malloc(sizeof(struct chunk));
+    chunk_p idat = malloc(sizeof(struct chunk));
+    chunk_p iend = malloc(sizeof(struct chunk));
+
+    /* Getting values for ihdr chunk */
+    int ihdr_start = 8;
+    U8 ihdr_type[4];
+    U32 ihdr_length = getValue(buffer, BASE, ihdr_start, ihdr_start + 4);
+    int index = 0;
+    /* ihdr type bytes should be 12 to 15 always */
+    for(int i = ihdr_start + 4; i < ihdr_start + 8; i++, index++){
+        ihdr_type[index] = buffer[i];
+    }
+    U32 ihdr_crc = getValue(buffer, BASE, ihdr_start + ihdr_length + 8, ihdr_start + ihdr_length + 12);
+
+    /* Getting values for idat chunk */
+    int idat_start = ihdr_start + ihdr_length + 12;
+    U8 idat_type[4];
+    U32 idat_length = getValue(buffer, BASE, idat_start, idat_start + 4);
+    index = 0;
+    for(int i = idat_start + 4; i < idat_start + 8; i++, index++){
+        idat_type[index] = buffer[i];
+    }
+    U32 idat_crc = getValue(buffer, BASE, idat_start + idat_length + 8, idat_start + idat_length + 12);
+
+    /* Getting values for iend chunk */
+    int iend_start = idat_start + idat_length + 12;
+    U8 iend_type[4];
+    U32 iend_length = getValue(buffer, BASE, iend_start, iend_start + 4);
+    index = 0; 
+    for(int i = iend_start + 4; i < iend_start + 8; i++, index++){
+        iend_type[index] = buffer[i];
+    }
+    U32 iend_crc = getValue(buffer, BASE, iend_start + 8, iend_start + 12);
+
+    ihdr->length = ihdr_length;
+    memcpy(ihdr->type, ihdr_type, sizeof(ihdr_type));
+    U8 *ihdrData = malloc(ihdr->length);
+    memcpy(ihdrData, buffer + ihdr_start + 8, ihdr->length);
+    ihdr->p_data = ihdrData;
+    ihdr->crc = ihdr_crc;
+
+    idat->length = idat_length;
+    memcpy(idat->type, idat_type, sizeof(idat_type));
+    U8 *idatData = malloc(idat->length);
+    memcpy(idatData, buffer + idat_start + 8, idat->length);
+    idat->p_data = idatData;
+    idat->crc = idat_crc;
+
+    iend->length = iend_length;
+    memcpy(iend->type, iend_type, sizeof(iend_type));
+    iend->p_data = NULL;
+    iend->crc = iend_crc;
+	
+    retVal->p_IHDR = ihdr;
+    retVal->p_IDAT = idat;
+    retVal->p_IEND = iend;
+    return retVal;
+}
+
+/**
+ * @brief writes a concatenated png to disk
+ * @param final_buffer, buffer with inflated image strips
+ * @param pngs simple_PNG_p *, array of png strips
+ * @param height unsigned int, final height
+ * @param width unsigned int, final width
+ */
+int catPNG(simple_PNG_p png, U8* final_buffer, U32 height, U32 width, int* offset){
+    int ret = 0;
+    int decompressed_size = height * (4 * width + 1);
+    U8 *decompressed_buffer = malloc(decompressed_size);
+    U64 new_size = 0;
+
+    ret = mem_def(decompressed_buffer, &new_size, final_buffer, (*offset * 9606), Z_DEFAULT_COMPRESSION);
+
+    if (ret == 0) { /* success */
+        // printf("original len = %d, len_def = %lu\n");
+    } else { /* failure */
+        fprintf(stderr,"mem_def failed. ret = %d.\n", ret);
+        return ret;
+    }
+
+    int marker = 0;
+
+    int new_buffer_size = PNG_SIG_SIZE + (3 * CHUNK_LEN_SIZE) + (3 * CHUNK_TYPE_SIZE) + (3 * CHUNK_CRC_SIZE) + DATA_IHDR_SIZE + new_size;
+
+    U8 *ihdr_buf = malloc(4 + DATA_IHDR_SIZE);
+    U8 *idat_buf = malloc(4 + new_size);
+    U8 *iend_buf = malloc(4);
+
+    U8 *new_height = insertValue(height, 4);
+    U8 *new_ihdr_data = malloc(DATA_IHDR_SIZE * sizeof(U8));
+    memcpy(new_ihdr_data, decompressed_buffer + PNG_SIG_SIZE + CHUNK_LEN_SIZE + CHUNK_TYPE_SIZE, DATA_IHDR_SIZE * sizeof(U8));
+    memcpy(new_ihdr_data + 4, new_height, 4 * sizeof(U8));
+
+    for(int i = 0; i < 4; i++){
+           ihdr_buf[i] = png->p_IHDR->type[i];
+           idat_buf[i] = png->p_IDAT->type[i];
+           iend_buf[i] = png->p_IEND->type[i];
+    }
+    int index = 0;
+    for(int i = 4; i < 4 + DATA_IHDR_SIZE; i++, index++){
+           ihdr_buf[i] = new_ihdr_data[index];
+    }
+    index = 0;
+    for(int i = 4; i < 4 + new_size; i++, index++){
+           idat_buf[i] = decompressed_buffer[index];
+    }
+
+    // U32 ihdr_crc_calc = crc(ihdr_buf, 4 + DATA_IHDR_SIZE);
+    U32 idat_crc_calc = crc(idat_buf, 4 + new_size);
+    U32 iend_crc_calc = crc(iend_buf, 4);
+
+    U8 concat_png[new_buffer_size];
+
+    U8 signature[8] = {0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A};
+    memcpy(concat_png, signature, sizeof(signature));
+    marker += PNG_SIG_SIZE;
+
+    U8 *new_ihdr_length = insertValue(13, 4);
+    memcpy(concat_png + marker, new_ihdr_length, (U8)sizeof(new_ihdr_length));
+    marker += CHUNK_LEN_SIZE;
+
+    memcpy(concat_png + marker, png->p_IHDR->type, sizeof(png->p_IHDR->type));
+    marker += CHUNK_TYPE_SIZE;
+
+    memcpy(concat_png + marker, png->p_IHDR->p_data, png->p_IHDR->length);
+
+    memcpy(concat_png + marker + 4, new_height, 4 * sizeof(U8));
+    marker += DATA_IHDR_SIZE;
+    U32 ihdr_crc_calc = crc(concat_png + PNG_SIG_SIZE + CHUNK_LEN_SIZE, 4 + DATA_IHDR_SIZE);
+
+    U8 *new_ihdr_crc = insertValue(ihdr_crc_calc, 4);
+    
+    memcpy(concat_png + marker, new_ihdr_crc, 4 * sizeof(U8));
+    marker += CHUNK_CRC_SIZE;
+
+    U8 *new_idat_length = insertValue(new_size, 4);
+    memcpy(concat_png + marker, new_idat_length, 4 * sizeof(U8));
+    marker += CHUNK_LEN_SIZE;
+
+    memcpy(concat_png + marker, png->p_IDAT->type, sizeof(png->p_IDAT->type));
+    marker += CHUNK_TYPE_SIZE;
+
+    memcpy(concat_png + marker, decompressed_buffer, new_size);
+    marker += new_size;
+
+    U8 *new_idat_crc = insertValue(idat_crc_calc, 4);
+    memcpy(concat_png + marker, new_idat_crc, 4 * sizeof(U8));
+    marker += CHUNK_CRC_SIZE;
+
+    U8 *new_iend_length = insertValue(0, 4);
+    memcpy(concat_png + marker, new_iend_length, 4 * sizeof(U8));
+    marker += CHUNK_LEN_SIZE;
+
+    memcpy(concat_png + marker, png->p_IEND->type, sizeof(png->p_IDAT->type));
+    marker += CHUNK_TYPE_SIZE;
+
+    // IEND data but its empty so do nothing
+
+    U8 *new_iend_crc = insertValue(iend_crc_calc, 4);
+    memcpy(concat_png + marker, new_iend_crc, 4 * sizeof(U8));
+    marker += CHUNK_CRC_SIZE;
+
+    FILE *fp;
+    fp = fopen("all.png", "w");
+    fwrite(concat_png, 1, sizeof(concat_png), fp);
+
+    fclose(fp);
+    
+    free(new_height);
+    free(new_ihdr_length);
+    free(new_ihdr_crc);
+    free(new_idat_length);
+    free(new_idat_crc);
+    free(new_iend_length);
+    free(new_iend_crc);
+
+    free(ihdr_buf);
+    free(idat_buf);
+    free(iend_buf);
+    free(decompressed_buffer);
+    free(new_ihdr_data);
+    return 0;
+}
+
+/**
+ * @brief inflates image strips to buffer
+ * @param pngs simple_PNG_p *, array of png strips
+ * @param final_buffer, buffer to inflate images to
+ */
+int inflateStrips(simple_PNG_p png, U8* final_buffer, int *offset){
+	U64 output_length = 0;
+	int ret = 0;
+    ret = mem_inf(final_buffer + (*offset * 9606), &output_length, png->p_IDAT->p_data, png->p_IDAT->length);
+    if (ret == 0) { /* success */
+    // printf("original len = %d, len_def = %lu, len_inf = %lu\n", final_size, pngs[i]->p_IDAT->length, output_length);
+    } else { /* failure */
+        fprintf(stderr,"mem_def failed. ret = %d.\n", ret);
+    }
+    //*offset += output_length;
+	return ret;
+}

982d4883254799f46fcf41e66d6aef30b1079c43
freeman-zhang <freemanzhang2@gmail.com>
2020-07-04 16:19:21 -0400

    pseudo code

--- lab4/starter/findpng2.c
+++ lab4/starter/findpng2.c
@@ -1,38 +1,72 @@
-#include <stdio.h>	            /* printf needs to include this header file */
-#include <stdlib.h>             /* for malloc()                 */
-#include <unistd.h>             /* for getopt                   */
-#include <getopt.h>             /* to get rid of optarg linting */
-#include <curl/curl.h>          /* for cURL                     */
-#include <pthread.h>            /* for pthread                  */
-#include "png_util/lab_png.h"   /* simple PNG data structures   */
-#include "cURL/curl_util.h"     /* for header and write cb      */
+#include <stdio.h>            /* printf needs to include this header file */
+#include <stdlib.h>           /* for malloc()                 */
+#include <unistd.h>           /* for getopt                   */
+#include <getopt.h>           /* to get rid of optarg linting */
+#include <curl/curl.h>        /* for cURL                     */
+#include <pthread.h>          /* for pthread                  */
+#include "png_util/lab_png.h" /* simple PNG data structures   */
+#include "cURL/curl_util.h"   /* for header and write cb      */
 
-int main(int argc, char **argv){
+void crawler(void *url)
+{
+    //dq a url from frontier
+
+    //curl call to url
+
+    //use libxml to traverse page,
+    //check links against pages visted:
+    // if on list, then ignore
+    // if not on list, then add to frontier
+
+    //check pngs against pngs list:
+    // if on list, ignore
+    // if not on list and pngs list not full, add to png list
+
+    //exit conditions:
+    // All threads are done and frontier empty
+    // pngs full
+
+    //repeat
+
+    //on exit, cleanup
+}
+
+int main(int argc, char **argv)
+{
     int c;
     int num_threads = 1;
     int num_urls = 50;
     char logfile = "";
     /* Maybe replace this with a call to main_getopt.c */
-    while ((c = getopt (argc, argv, "t:m:")) != -1) {
-        switch (c) {
+    while ((c = getopt(argc, argv, "t:m:")) != -1)
+    {
+        switch (c)
+        {
         case 't':
             num_threads = strtoul(optarg, NULL, 10);
-            if (num_threads <= 0) {
-                    fprintf(stderr, "%s: -t > 0 -- 't'\n", argv[0]);
-                    return -1;
-                }
-        break;
+            if (num_threads <= 0)
+            {
+                fprintf(stderr, "%s: -t > 0 -- 't'\n", argv[0]);
+                return -1;
+            }
+            break;
         case 'm':
             num_urls = strtoul(optarg, NULL, 10);
-            if (num_urls <= 0) {
+            if (num_urls <= 0)
+            {
                 fprintf(stderr, "%s: -m > 0 -- 'n'\n", argv[0]);
                 return -1;
             }
-        break;
+            break;
         default:
             return -1;
         }
     }
+
+    //use libxml, traverse the seed url, set up frontier
+    //add seed url to pages visited
+
+    //create threads
 
     return 0;
 }
